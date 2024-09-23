fda5f05ad41af722cac7bf34ee30334c23fdd973
JoaquinReyes2905 <Mikemoble2905@gmail.com>
2024-09-23 19:49:19 -0300

    connection with database

--- Backend/application-log/src/main/java/com/web/app/ApplicationLogApplication.java
+++ Backend/application-log/src/main/java/com/web/app/ApplicationLogApplication.java
@@ -1,13 +1,13 @@
-package com.login;
+package com.web.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginApplication {
+public class ApplicationLogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
+		SpringApplication.run(ApplicationLogApplication.class, args);
 	}
 
 }
--- Backend/application-log/src/test/java/com/web/app/ApplicationLogApplicationTests.java
+++ Backend/application-log/src/test/java/com/web/app/ApplicationLogApplicationTests.java
@@ -1,10 +1,10 @@
-package com.login;
+package com.web.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoginApplicationTests {
+class ApplicationLogApplicationTests {
 
 	@Test
 	void contextLoads() {
